370443ac3f1406f2b3d648d5fdfde8859c586f6c
Zoey425 <108717962+Zoey425@users.noreply.github.com>
2024-03-28 23:59:04 +0900

    update product detail page

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Button, Col, Container, Row, Dropdown } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 
 const ProductDetail = () => {
     let { id } = useParams();
-    const [productDetail, setProductDetail] = useState([]);
+    const [productDetail, setProductDetail] = useState({});
     const getProductDetail = async () => {
         let url = `http://localhost:3004/products/${id}`;
         let response = await fetch(url);
@@ -21,8 +21,27 @@ const ProductDetail = () => {
                     <img src={productDetail?.img} alt={productDetail?.title} />
                 </Col>
                 <Col>
-                    <div>{productDetail?.title}</div>
-                    <div>{productDetail?.price}</div>
+                    <div className="detail-title">{productDetail?.title}</div>
+                    <div className="detail-price">{productDetail?.price} 원</div>
+                    <div>
+                        <Dropdown>
+                            <Dropdown.Toggle variant="success" id="dropdown-basic">
+                                Size
+                            </Dropdown.Toggle>
+
+                            <Dropdown.Menu>
+                                {Array.isArray(productDetail.size) &&
+                                    productDetail.size.map((item, index) => (
+                                        <Dropdown.Item key={index} href={item}>
+                                            {item}
+                                        </Dropdown.Item>
+                                    ))}
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </div>
+                    <Button variant="dark" className="detail-get-button">
+                        구매하기
+                    </Button>
                 </Col>
             </Row>
         </Container>
